2f6cd8b5d2717ee6e9990788a7e26f012408a574
Lucca Viccini <lucca.viccini@engenharia.ufjf.br>
2022-10-17 11:41:23 -0300

    icone check habilitado

--- driveneats.js
+++ driveneats.js
@@ -1,44 +1,44 @@
-let comida_check, bebida_check, sobremesa_check = null;
+let comida_check,
+  bebida_check,
+  sobremesa_check = null;
 
 function selecionar(selecionado) {
-    const nodes = selecionado.parentElement.getElementsByTagName("div");
-    console.log(nodes.length);
-    for (let i = 0; i < nodes.length; i++) {
-        nodes[i].classList.remove('selecionado');
-    }     
-    selecionado.classList.add('selecionado');
-    
-    
-    
-    enable_botao(selecionado);   
-} 
+  const nodes = selecionado.parentElement.getElementsByTagName("div");
+  const botao_check = selecionado.querySelector("ion-icon");
 
-function alterarBotao(){
-    const botao = document.getElementById("botao");
-    botao.disabled = false;
-    botao.textContent = 'Fechar pedido';
-    botao.style.backgroundColor = '#32B72F';
+  for (let i = 0; i < nodes.length; i++) {
+    nodes[i].classList.remove("selecionado");
+    nodes[i].querySelector("ion-icon").classList.remove("show");
+  }
 
-}  
+  selecionado.classList.add("selecionado");
+  botao_check.classList.add("show");
 
-function enable_botao(selecionado){
-    if (selecionado.parentElement.classList.contains('container-comida')){
-        comida_check = true;
-        console.log(comida_check);
-    
-    }
-    if (selecionado.parentElement.classList.contains('container-bebida')){
-        bebida_check = true;
-        console.log(bebida_check);
-    }
-    if (selecionado.parentElement.classList.contains('container-sobremesa')){
-        sobremesa_check = true;
-        console.log(sobremesa_check);
-    }
+  enable_botao(selecionado);
+}
 
-    if((comida_check == true) && (bebida_check==true)&& sobremesa_check==true){
-        alterarBotao()
-    }
+function alterarBotao() {
+  const botao = document.getElementById("botao");
+  botao.disabled = false;
+  botao.textContent = "Fechar pedido";
+  botao.style.backgroundColor = "#32B72F";
+}
 
+function enable_botao(selecionado) {
+  if (selecionado.parentElement.classList.contains("container-comida")) {
+    comida_check = true;
+    console.log(comida_check);
+  }
+  if (selecionado.parentElement.classList.contains("container-bebida")) {
+    bebida_check = true;
+    console.log(bebida_check);
+  }
+  if (selecionado.parentElement.classList.contains("container-sobremesa")) {
+    sobremesa_check = true;
+    console.log(sobremesa_check);
+  }
 
+  if (comida_check == true && bebida_check == true && sobremesa_check == true) {
+    alterarBotao();
+  }
 }
